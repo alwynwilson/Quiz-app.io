39a2991752645812c580a8908d81b1f3f1fc4b0b
Alwyn <alwynwilson187@gmail.com>
2024-05-08 16:34:17 +0530

    project completed

---
+++ src/assets/data.js
@@ -0,0 +1,83 @@
+export const data = [
+    {
+        question:"HTML stands for",
+        option1:"HighText Machine Language",
+        option2:"HyperText and links Markup Language",
+        option3:"HyperText Markup Language",
+        option4:"None of these",
+        ans:3
+    },
+    {
+        question:"Which of the following element is responsible for making the text bold in HTML?",
+        option1:"<pre>",
+        option2:"<a>",
+        option3:"<b>",
+        option4:"<br>",
+        ans:3
+    },
+    {
+        question:"Which of the following tag is used for inserting the largest heading in HTML?",
+        option1:"<h3>",
+        option2:"<h1>",
+        option3:"<h5>",
+        option4:"<h6>",
+        ans:2
+    },
+    {
+        question:" Which of the following tag is used to insert a line-break in HTML?",
+        option1:"<br>",
+        option2:"<a>",
+        option3:"<pre>",
+        option4:"<b>",
+        ans:1
+    },
+    {
+        question:"How to create an unordered list (a list with the list items in bullets) in HTML?",
+        option1:"<ul>",
+        option2:"<ol>",
+        option3:"<li>",
+        option4:"<i>",
+        ans:1
+    },
+    {
+        question:" How to create an ordered list (a list with the list items in numbers) in HTML?",
+        option1:"<ul>",
+        option2:"<ol>",
+        option3:"<li>",
+        option4:"<i>",
+        ans:2
+    },
+    {
+        question:"Which of the following element is responsible for making the text italic in HTML?",
+        option1:"<i>",
+        option2:"<italic>",
+        option3:"<it>",
+        option4:"<pre>",
+        ans:1
+    },
+    {
+        question:"How to insert an image in HTML?",
+        option1:"<img href = `jtp.png` />",
+        option2:"<img url = `jtp.png` />",
+        option3:"<img link = `jtp.png` />",
+        option4:"<img src = `jtp.png` />",
+        ans:4
+    },
+    {
+        question:"How to create a checkbox in HTML?",
+        option1:"<input type = `checkbox`>",
+        option2:"<input type = `button`>",
+        option3:"<checkbox>",
+        option4:"<input type = `check`>",
+        ans:1
+    },
+    {
+        question:"Which of the following tag is used to define options in a drop-down selection list?",
+        option1:"<select>",
+        option2:"<list>",
+        option3:"<dropdown>",
+        option4:"<option>",
+        ans:4
+    }
+
+]
